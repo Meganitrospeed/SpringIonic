630ae4fed699563011a33e8013c601e9bade45aa
tcrurav <tiburcio.cruz@gmail.com>
2020-09-25 19:44:39 +0100

    added src folder

---
+++ frontend/src/app/list/list.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Bicycle } from '../models/bicycle';
+import { BicycleService } from '../services/bicycle.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+})
+export class ListPage implements OnInit {
+
+  bicycles: Bicycle[];
+
+  constructor(private bicycleService: BicycleService) { }
+
+  ngOnInit() {
+    
+  }
+
+  getAllBicycles(){
+    this.bicycleService.getBicycles().subscribe( bicycles => {
+      this.bicycles = bicycles;
+    });
+
+  }
+
+}
---
+++ frontend/src/app/services/bicycle.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Bicycle } from '../models/bicycle';
+import { Observable, of } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'http://localhost:8080/api/bicycles';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BicycleService {
+
+  constructor(private http: HttpClient) { }
+
+  getBicycles(): Observable<Bicycle[]> {
+    return this.http.get<Bicycle[]>(apiUrl);
+      // .pipe(
+      //   tap(bicycle => console.log('fetched bicycles'))
+      //   // ,
+      //   // catchError(this.handleError('getBicycles', []))
+      // );
+  }
+
+  // private handleError<T>(operation = 'operation', result?: T) {
+  //   return (error: any): Observable<T> => {
+  //     console.error(error); // log to console instead
+  //     return of(result as T);
+  //   };
+  // }
+}
